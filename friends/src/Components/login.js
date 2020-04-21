a3f4334e060bb923513efa924abbd632be840e9f
GeraldRyan <gerald.ryan40@yahoo.com>
2020-04-21 15:08:40 -0500

    forward

--- friends/src/Components/login.js
+++ friends/src/Components/login.js
@@ -4,6 +4,13 @@ import React from 'react'
 
 export function Login()
 {
+
+  const handleClick = () =>
+  {
+
+  }
+
+
   return (
     <div>
       <div>
@@ -13,6 +20,10 @@ export function Login()
       <div>
         <label htmlFor="password">UserName </label>
         <input type="password" name='password' placeholder="enter your password" />
+      </div>
+      <br/>
+      <div>
+        <button onClick='handleClick'>Submit!</button>
       </div>
     </div>
 
